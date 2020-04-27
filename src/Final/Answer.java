d8c3cb66f34791ebd4cc4f1071da8700c87c53ab
RXC0765 <63360122+RXC0765@users.noreply.github.com>
2020-04-26 20:24:25 -0400

    Update Answer.java

--- src/Final/Answer.java
+++ src/Final/Answer.java
@@ -8,54 +8,43 @@ public class Answer {
 	ArrayList<User> users;
 	
 	public Answer() {
-		this.users = new ArrayList<User>(); //Create an array list so that multiple users can take quiz.
-	}
-	
-	public void addUser(User player) {
-		users.add(player);
-	}
-	
-	public void PrintResult() {
-		for(int i=0; i< users.size(); i++) {
-			System.out.println(users.get(i).toString());
-		}
+		users = new ArrayList<User>();
 	}
 	
 	public static void main(String[] args) throws IOException {
 		System.out.println("Welcome to the Quiz App!");
 		System.out.println("You bring the brains, we'll bring everything else!");
-		Scanner input = new Scanner(System.in);
-		Answer answer = new Answer();
-		String start = "";
 		
+		String start = "";
+		int newplayer=1;
 		while(true) {
 		    System.out.println("Do you want to start you vocabulary learning journey? (Y/N)");
+		    Scanner input = new Scanner(System.in);
 		    start = input.nextLine();
 		    
 		    if(start.compareToIgnoreCase("Y")==0) {
-		    	System.out.println("Please enter your name:");
-		    	String name = input.nextLine();
-		        
-		        User player = new User(name); //Create a user
-		        answer.addUser(player); //Add it to the array list
-		        
-		        System.out.println("Hello "+name+" ! Let's get started");
-		        QuestionBank qb=new QuestionBank();
-		        qb.openTestFile(new File("test2.txt"));   //open the file: a GRE vocabulary quiz
-		        player.ChangeScore(qb.score); //Update the score in the user class
-		        
+		    	while(start.compareToIgnoreCase("Y")==0) {
+			    	System.out.println("Please enter your name:");
+			    	String name = input.nextLine();
+			        
+			        User user = new User(name);
+			        Answer answer = new Answer();
+			        answer.users.add(user); 
+			        
+			        System.out.println("Hello "+name+" ! Let's get started");
+			        QuestionBank qb=new QuestionBank();
+			        qb.openTestFile(new File("test.txt"));   //open the file
+			        user.ChangeScore(qb.score); 
+			        
+			        System.out.println("Is there another player? (1/0)");
+			        newplayer=Integer.parseInt(input.nextLine());
+		    	}		        
 		    }else if(start.compareToIgnoreCase("N")==0) {
-		    	if(answer.users.size()>0) {
-		    		System.out.println("In summary: ");
-		    		answer.PrintResult();
-		    	}
 		    	System.out.println("Bye!");
 		    	break;
-		    	
 		    }else {
-		    	System.out.println("Please enter Y or N"); 
-		    }	    
-		}
-	}
-	
+		    	System.out.println("Please enter Y or N");
+		    }
+		}			
+	}	
 }
