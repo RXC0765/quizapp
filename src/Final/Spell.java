718f4c9047c8aa361b0150c264663d1d3c6852c2
SHIYI MAO <44790654+mao2021@users.noreply.github.com>
2020-04-28 17:57:42 -0400

    updated

--- src/Final/Spell.java
+++ src/Final/Spell.java
@@ -9,31 +9,51 @@ import java.io.FileNotFoundException;
  * @author Yinghan Lin
  * This is a subclass of StudyMode.class
  */
-public class Spell extends StudyMode {
+public class Spell extends AbstractStudyMode implements CanPlay {
 	
 	public Spell(File f) throws FileNotFoundException { //constructor 
-		super(f); 
+		super(f);
+		loadFromFile();
 	}
-    
-	public void beginExerciseSpell() {
+
+	@Override
+	protected String processQuestion(String question) {
+		return question;
+	}
+
+	@Override
+	protected String processAnswer(String answer) {
+		answer = answer.replace("-","");//pick out the answer
+		answer = answer.trim();
+		return answer;
+
+	}
+
+	@Override
+	public void beginExercise() {
 		System.out.println("Welcome to the Spell section! In this section, you will have to spell");
 		System.out.println("the correct word according to the definitions given.");
 		System.out.println();
 		
 		Scanner input = new Scanner(System.in);
-		Set<String> questionSet = exercise.keySet(); //get all the key values: questions
+		Set<String> questionSet = getQuestions(); //get all the key values: questions
 		for(String question: questionSet) {
 			System.out.println(question);
 			System.out.println("Please type your answer:");
 			String a = input.nextLine();
-			if(a.toLowerCase().equals(exercise.get(question))) { //check if the answer is correct
+			//if the answer is wrong, show the right answer
+			if(verifyAnswer(question, a)) { //check if the answer is correct
                 System.out.println("Correct!");
-                System.out.println();
 			}else{
-				System.out.println("Wrong! The answer is "+exercise.get(question)); 
-				System.out.println(); //if the answer is wrong, show the right answer
+				System.out.println("Wrong! The answer is "+getAnswer(question));
 			}
+			System.out.println();
 		}
 	}
-	
+
+	@Override
+	public boolean verifyAnswer(String question, String answer) {
+	    answer = answer.toLowerCase();
+	    return super.verifyAnswer(question, answer);
+	}
 }
